cfcd5d85f396828d7ba7424fd005211ecd41b9a5
Dan Carp <carpdan@amazon.com>
2022-08-13 08:12:03 +0300

    running sum of 1d array

---
+++ src/main/java/com/danandreicarp/arrays/RunningSumOf1DArray.java
@@ -0,0 +1,11 @@
+package com.danandreicarp.arrays;
+
+public class RunningSumOf1DArray {
+
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
---
+++ src/test/java/com/danandreicarp/arrays/RunningSumOf1DArrayTest.java
@@ -0,0 +1,42 @@
+package com.danandreicarp.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RunningSumOf1DArrayTest {
+
+    RunningSumOf1DArray app = new RunningSumOf1DArray();
+
+    @Test
+    void testCase1() {
+        int[] result = app.runningSum(new int[]{1, 2, 3, 4});
+        assertEquals(4, result.length);
+        assertEquals(1, result[0]);
+        assertEquals(3, result[1]);
+        assertEquals(6, result[2]);
+        assertEquals(10, result[3]);
+    }
+
+    @Test
+    void testCase2() {
+        int[] result = app.runningSum(new int[]{1, 1, 1, 1, 1});
+        assertEquals(5, result.length);
+        assertEquals(1, result[0]);
+        assertEquals(2, result[1]);
+        assertEquals(3, result[2]);
+        assertEquals(4, result[3]);
+        assertEquals(5, result[4]);
+    }
+
+    @Test
+    void testCase3() {
+        int[] result = app.runningSum(new int[]{3, 1, 2, 10, 1});
+        assertEquals(5, result.length);
+        assertEquals(3, result[0]);
+        assertEquals(4, result[1]);
+        assertEquals(6, result[2]);
+        assertEquals(16, result[3]);
+        assertEquals(17, result[4]);
+    }
+}
