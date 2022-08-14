26d279b86e3db4c0e30e3cd49b176ccbda27900f
danandreicarp <carpdan@pm.me>
2022-08-14 19:14:49 +0300

    number of steps to reduce number to zero take 3

--- src/main/java/com/danandreicarp/number/NumberOfStepsToReduceNumberToZero.java
+++ src/main/java/com/danandreicarp/number/NumberOfStepsToReduceNumberToZero.java
@@ -3,13 +3,9 @@ package com.danandreicarp.number;
 public class NumberOfStepsToReduceNumberToZero {
 
     public int numberOfSteps(int num) {
-        int count = 0;
-        while (num > 0) {
-            if ((num & 1) == 0) num >>= 1;
-            else num -= 1;
-            ++count;
-        }
-
-        return count;
+        String binary = Integer.toBinaryString(num);
+        int length = binary.length();
+        int ones = Integer.bitCount(num);
+        return (length - 1) + ones;
     }
 }
