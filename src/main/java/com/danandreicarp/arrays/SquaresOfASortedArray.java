5132814fb7e439bf6de921dfd2d32d087bcc5352
danandreicarp <carpdan@pm.me>
2022-08-21 12:47:25 +0300

    squares of a sorted array take 2

--- src/main/java/com/danandreicarp/arrays/SquaresOfASortedArray.java
+++ src/main/java/com/danandreicarp/arrays/SquaresOfASortedArray.java
@@ -1,14 +1,42 @@
 package com.danandreicarp.arrays;
 
-import java.util.Arrays;
-
 public class SquaresOfASortedArray {
     public int[] sortedSquares(int[] nums) {
-        boolean needsSort = nums[0] < 0;
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = nums[i] * nums[i];
+        int[] output = new int[nums.length];
+
+        int i = 0;
+        int j;
+        while (i < nums.length) {
+            int vi = Math.abs(nums[i]);
+            int index = 0;
+            int shift = 0;
+            int count = 0;
+            if (nums[i] < 0) {
+                // count the numbers less than this, going forward
+                for (j = i + 1; j < nums.length; j++) {
+                    if (vi > Math.abs(nums[j])) ++count;
+                    // not using 'abs' here to avoid hitting the same index in a [-3, 1, 3] scenario
+                    else if (nums[i] == nums[j]) ++shift;
+                    // no other values will be less than this going forward
+                    else if (vi < nums[j]) break;
+                }
+                index = count + shift;
+            } else {
+                // count the numbers less than this, going backward
+                for (j = i - 1; j >= 0; j--) {
+                    int vj = Math.abs(nums[j]);
+                    if (vi > vj) ++count;
+                    // using 'abs' here to count all the matching numbers
+                    else if (vi == vj) ++shift;
+                    // no other values will be less than this going backward
+                    else break;
+                }
+                index = count + shift;
+            }
+            output[index] = vi * vi;
+            ++i;
         }
-        if (needsSort) Arrays.sort(nums);
-        return nums;
+
+        return output;
     }
 }
--- src/test/java/com/danandreicarp/arrays/SquaresOfASortedArrayTest.java
+++ src/test/java/com/danandreicarp/arrays/SquaresOfASortedArrayTest.java
@@ -29,4 +29,53 @@ class SquaresOfASortedArrayTest {
         assertEquals(49, output[3]);
         assertEquals(121, output[4]);
     }
+
+    @Test
+    void testCase3() {
+        int[] output = app.sortedSquares(new int[]{-7, -3, 4, 5, 11});
+        assertEquals(5, output.length);
+        assertEquals(9, output[0]);
+        assertEquals(16, output[1]);
+        assertEquals(25, output[2]);
+        assertEquals(49, output[3]);
+        assertEquals(121, output[4]);
+    }
+
+    @Test
+    void testCase4() {
+        int[] output = app.sortedSquares(new int[]{-10, -1, 0, 3, 4});
+        assertEquals(5, output.length);
+        assertEquals(0, output[0]);
+        assertEquals(1, output[1]);
+        assertEquals(9, output[2]);
+        assertEquals(16, output[3]);
+        assertEquals(100, output[4]);
+    }
+
+    @Test
+    void testCase5() {
+        int[] output = app.sortedSquares(new int[]{-4, -4, -3});
+        assertEquals(3, output.length);
+        assertEquals(9, output[0]);
+        assertEquals(16, output[1]);
+        assertEquals(16, output[2]);
+    }
+
+    @Test
+    void testCase6() {
+        int[] output = app.sortedSquares(new int[]{-5, 4, 4});
+        assertEquals(3, output.length);
+        assertEquals(16, output[0]);
+        assertEquals(16, output[1]);
+        assertEquals(25, output[2]);
+    }
+
+    @Test
+    void testCase7() {
+        int[] output = app.sortedSquares(new int[]{1, 3, 3});
+        assertEquals(3, output.length);
+        assertEquals(1, output[0]);
+        assertEquals(9, output[1]);
+        assertEquals(9, output[2]);
+    }
 }
